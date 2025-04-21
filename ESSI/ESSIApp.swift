ea55250f9fc47f6383e7217d04ac21946983faa0
Robert Beachill <bbeachill@icloud.com>
2025-04-21 22:26:45 +0100

    Initial Commit

---
+++ ESSI/ESSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESSIApp.swift
+//  ESSI
+//
+//  Created by Robert Beachill on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ESSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
